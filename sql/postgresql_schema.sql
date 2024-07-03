b5e1cc19aa66127b822c8b831d15cb48e8a63d9d
MehmetYukselSekeroglu <dijital_evren@protonmail.com>
2024-07-03 09:48:15 +0300

    bug fix

--- sql/postgresql_schema.sql
+++ sql/postgresql_schema.sql
@@ -1,21 +1,21 @@
-CREATE TABLE IF NOT EXISTS {"system_core"} (
+CREATE TABLE IF NOT EXISTS "system_core" (
      id SERIAL PRIMARY KEY,
      unique_key VARCHAR(100) NOT NULL UNIQUE,
      value VARCHAR(100) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS {"blob_storage"} (
+CREATE TABLE IF NOT EXISTS "blob_storage" (
     id SERIAL PRIMARY KEY,
     unique_blob_key TEXT NOT NULL UNIQUE,
     key_value BYTEA NOT NULL,
     data_type TEXT NOT NULL DEFAULT 'user',
     information_notes TEXT DEFAULT NULL
 );
-CREATE TABLE IF NOT EXISTS {"authentication"} (
+CREATE TABLE IF NOT EXISTS "authentication" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(128) NOT NULL UNIQUE,
     password VARCHAR(128) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS {"face_recognition_standart"} (
+CREATE TABLE IF NOT EXISTS "face_recognition_standart" (
     id SERIAL PRIMARY KEY,
     face_picture_blob BYTEA NOT NULL, 
     picture_sha1_hash TEXT NOT NULL UNIQUE,
